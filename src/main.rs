2c304a60ed0265b8175175d4ef0231af63d28cfc
Ahmad Urabi <hamod9041@gmail.com>
2024-06-23 12:08:42 +0300

    Added config file parameter / Unteseted

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::io;
+use std::io::BufRead;
 use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
 use std::collections::HashMap;
@@ -5,7 +8,7 @@ use std::collections::HashMap;
 use clap::Parser;
 
 #[cfg(target_os = "windows")]
-fn sort_files(files: Vec<PathBuf>, map: HashMap<&str, &str>, base: &str) {
+fn sort_files(files: Vec<PathBuf>, map: HashMap<String, String>, base: &str) {
     for i in files {
         let ext = i.extension();
         if ext.is_none() {
@@ -32,7 +35,7 @@ fn sort_files(files: Vec<PathBuf>, map: HashMap<&str, &str>, base: &str) {
 // TODO: Recursive search, directories with a single file type inside parent dir
 
 #[cfg(not(target_os = "windows"))]
-fn sort_files(files: Vec<PathBuf>, map: HashMap<&str, &str>, base: &str) {
+fn sort_files(files: Vec<PathBuf>, map: HashMap<String, String>, base: &str) {
     for i in files {
         let ext = i.extension();
         if ext.is_none() {
@@ -66,7 +69,64 @@ struct Args {
     #[arg(short, long, value_name = "Ignore Hidden Files")]
     ignore_hidden: Option<bool>,
     #[arg(short, long, value_name = "Ignore SymLinks Files")]
-    ignore_symlink: Option<bool>
+    ignore_symlink: Option<bool>,
+    #[arg(short, long, value_name = "Configuration File")]
+    config_file: Option<PathBuf>
+}
+
+
+
+fn read_config(config: PathBuf) -> Result<HashMap<String, String>, std::io::Error> {
+    if !config.is_file() {
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Config file is not valid"));
+    }
+
+    // Parsing Configuration
+    // File Format is as follows:
+    // extenstion:category
+    
+    let file = fs::File::open(config)?;
+    let reader = std::io::BufReader::new(file);
+    let mut res: HashMap<String, String> = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let split: Vec<&str> = line.split(':').collect();
+        if split.len() == 2 {
+            res.insert(split[0].to_string(), split[1].to_string());
+        } else {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid line format in config file"));
+        }
+    }
+
+    Ok(res)
+}
+
+
+fn read_default_config() -> HashMap<String, String> {
+    // Return default hardcoded Configuration
+    HashMap::from([
+        ("pdf".to_string(), "Documents".to_string()),
+        ("txt".to_string(), "Documents".to_string()),
+        ("docx".to_string(), "Documents".to_string()),
+        ("doc".to_string(), "Documents".to_string()),
+        ("ppt".to_string(), "Slides".to_string()),
+        ("pptx".to_string(), "Slides".to_string()),
+        ("png".to_string(), "Pictures".to_string()),
+        ("jpg".to_string(), "Pictures".to_string()),
+        ("jpeg".to_string(), "Pictures".to_string()),
+        ("csv".to_string(), "Spreadsheets".to_string()),
+        ("xlsx".to_string(), "Spreadsheets".to_string()),
+        ("xls".to_string(), "Spreadsheets".to_string()),
+        ("zip".to_string(), "Compressed".to_string()),
+        ("rar".to_string(), "Compressed".to_string()),
+        ("tar".to_string(), "Compressed".to_string()),
+        ("gz".to_string(), "Compressed".to_string()),
+        ("7z".to_string(), "Compressed".to_string()),
+        ("dmg".to_string(), "Applications".to_string()),
+        ("exe".to_string(), "Applications".to_string()),
+        ("app".to_string(), "Applications".to_string()),
+    ])
 }
 
 fn main() {
@@ -86,8 +146,18 @@ fn main() {
 
     let now = std::time::Instant::now();
 
+    let config: HashMap<String, String> = match args.config_file {
+        Some(file) => {
+            match read_config(file) {
+                Ok(conf) => conf,
+                Err(_e) => read_default_config()
+            }
+        },
+        None => read_default_config()
+    };
+
     // TODO: Extract to config file
-    let map: HashMap<&str, &str> = HashMap::from([
+    /*let map: HashMap<&str, &str> = HashMap::from([
         ("pdf","Documents"),
         ("txt","Documents"),
         ("docx","Documents"),
@@ -108,13 +178,13 @@ fn main() {
         ("dmg", "Applications"),
         ("exe", "Applications"),
         ("app", "Applications")
-    ]);
+    ]);*/
     let files = read_folder(&mut base, ignore_hidden, ignore_symlink);
     if files.is_err() {
         return;
     }
     let files = files.unwrap();
-    sort_files(files, map, base.to_str().unwrap());
+    sort_files(files, config, base.to_str().unwrap());
 
     println!("Finished in {}ms", now.elapsed().as_millis());
 }
